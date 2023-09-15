0506f9dd2e64c040b7f017ce7a874bf237a8d329
hasan <mmhotmail>
2023-09-15 15:22:10 +0300

    header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.scss";
+const Header = () => {
+  return (
+    <div className="header">
+      <span className="text-shadow">What is your horoscope?</span>
+    </div>
+  );
+};
+
+export default Header;
